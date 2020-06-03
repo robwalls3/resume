458d28faf9f44e4b133cf659ea186ed259584436
Rob Walls <rob.w3@pm.me>
2020-06-03 14:04:55 -0600

    [GitFast Update] Message: test message

--- gitFast.sh
+++ gitFast.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 source ./bash.env
 
-$MESSAGE = $1
-
 git add --all
-git commit -am "[GitFast Update] Message: $MESSAGE"
+git commit -am "[GitFast Update] Message: $1"
 git push -u $GITHUB_USER master
