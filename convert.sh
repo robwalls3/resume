94ba19c19f1647f25d2be15c369dace572970666
Rob Walls <rob.w3@pm.me>
2020-06-03 11:40:13 -0600

    Slammed doors and stomped on ceilings

---
+++ convert.sh
@@ -0,0 +1,2 @@
+curl --form file=@resume.docx http://localhost:7080/unconv/pdf > latestResume.pdf
+
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 isRunning=$(docker ps | grep 7080 | awk 'NR==1{print $2}')
+source ./bash.env
 
 pat="unoconv"
 
@@ -6,5 +7,5 @@ echo "Running: $isRunning"
 if [[ $isRunning =~ $pat ]]; then
   echo "Already running"
 else
-  docker run --rm -d -p 7080:3000 --env-file=docker.env -v /opt/unoconv:/opt/unoconvservice/status sfoxdev/unoconv
+  docker run --rm -network jenkins_network -d -p 7080:3000 --env-file=docker.env -v /opt/unoconv:/opt/unoconvservice/status sfoxdev/unoconv
 fi
